5d393e5cdf2805a561b89cacbe9947a79c179363
pcarcal254 <pcarcal254@g.educaand.es>
2022-03-09 19:57:35 +0100

    v2_Arreglos

--- src/metodos/programa.java
+++ src/metodos/programa.java
@@ -362,7 +362,10 @@ public class programa {
 		} else {
 			System.out.println("ESTE VEHICULO YA HA SIDO ALQUILADO, LO SENTIMOS MUCHO");
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
+=======
+>>>>>>> Stashed changes
 		}
 	}
 	
